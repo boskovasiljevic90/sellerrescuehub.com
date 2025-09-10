15b435fe6245031e9d39673b5390be427a265cb1
boskovasiljevic90 <boskovasiljevic90@gmail.com>
2025-09-10 21:46:34 +0200

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,4 +1,4 @@
-import { clsx } from "clsx";
+import clsx from "clsx";
 
 export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return <div className={clsx("rounded-2xl border bg-white p-6 shadow-sm", className)} {...props} />;
