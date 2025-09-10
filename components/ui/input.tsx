c3f5fe4397043dc96e88ffe7f7c27244e6bc9a24
boskovasiljevic90 <boskovasiljevic90@gmail.com>
2025-09-10 21:16:56 +0200

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,20 @@
+import * as React from "react";
+import { clsx } from "clsx";
 
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
+  { className, ...props },
+  ref
+) {
+  return (
+    <input
+      ref={ref}
+      className={clsx(
+        "block w-full rounded-xl border border-slate-200 bg-white px-3 py-2 text-sm shadow-sm outline-none placeholder:text-slate-400 focus:border-brand focus:ring-2 focus:ring-brand",
+        className
+      )}
+      {...props}
+    />
+  );
+});
